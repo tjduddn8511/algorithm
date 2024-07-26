db7bee7060bb98c19ffd449350d85bb27c5af8f1
서영우 <t01092127143@gmail.com>
2024-07-26 23:57:09 +0900

    입력 받아봄

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        int A;
+        int B;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A+B);
     }
 }
